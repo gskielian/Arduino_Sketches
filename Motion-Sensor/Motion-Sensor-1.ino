2314799d3143040f35e00f286da930f6c71b39d7
gskielian <gregory.kielian@hackerdojo.com>
2013-10-22 13:16:38 -0700

    Create Motion-Sensor-1.ino

---
+++ Motion-Sensor/Motion-Sensor-1.ino
@@ -0,0 +1,23 @@
+
+//Create a place to store the PIR sensor value
+
+//value will be 
+//"1" when high 
+// or
+//"0" when low
+
+int sensorValue;
+
+void setup() {
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  Serial.println("Hello Serial");
+  
+  delay(10);
+
+}
