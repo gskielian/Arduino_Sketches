10d72750ff475ced804629070d7c563a50f51ceb
gskielian <gregory.kielian@hackerdojo.com>
2013-10-22 19:13:07 -0700

    Update Motion-Sensor-3.ino

--- Motion-Sensor/Motion-Sensor-3.ino
+++ Motion-Sensor/Motion-Sensor-3.ino
@@ -1,1 +1,27 @@
 //Create an alarm here using what you have learned so far (code from previous example included for convenience)
+
+
+int someValue=35;
+
+
+
+void setup() {
+
+  Serial.begin(9600);
+  
+  pinMode(9,INPUT);
+
+  //Hint: here you'll need to make some pin an OUTPUT
+}
+
+void loop() {
+
+  someValue = digitalRead(9);
+  Serial.println("Hello Serial");
+  Serial.println(someValue);  
+  
+  //Hint: You'll need to place an if-else statement around here
+  
+  delay(10);
+
+}
